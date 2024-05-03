48ad7533a5b547b7fab6e2099dd7541625a0b872
Tiagoblima <tiago.blima02@gmail.com>
2024-05-03 08:01:35 -0300

    adding vocab file and get third party

---
+++ get_third_party.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/moses-smt/mosesdecoder.git third_party/mosesdecoder
+git clone https://github.com/facebookresearch/MUSE.git third_party/MUSE
+git clone https://github.com/glample/fastBPE.git third_party/fastBPE
