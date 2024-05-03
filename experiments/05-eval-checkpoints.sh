6a2793203e1068835cafb0d0534d39ddac459263
Tiagoblima <tiago.blima02@gmail.com>
2024-05-03 08:17:15 -0300

    fix eval

--- experiments/05-eval-checkpoints.sh
+++ experiments/05-eval-checkpoints.sh
@@ -1,23 +1,23 @@
 VALIDSTEPS=2000
 TRAINSTEPS=350000
-SAVESTEPS=10000
+SAVESTEPS=50000
 SRC_LANG=$1
 STD_LANG=$2
 SRCSTD_MODELDIR=$SRC_LANG-$STD_LANG/checkpoints
-for i in $(eval echo {0..${TRAINSTEPS}..${SAVESTEPS}}); do
+for i in $(eval echo {${SAVESTEPS}..${TRAINSTEPS}..${SAVESTEPS}}); do
     if [ -f $SRCSTD_MODELDIR/checkpoint_step_${i}.pt ]; then 
         echo "Evaluating checkpoint ... ${i}"
         python -u ../translate.py\
             -gpu 0\
             -model $SRCSTD_MODELDIR/checkpoint_step_${i}.pt\
-            -src $SRC_LANG-$STD_LANG/bpetok/valid.$SRC_LANG.bpetok\
+            -src ../data/preprocessed/texts/nllb/$SRC_LANG/validation.txt \
             -output $SRCSTD_MODELDIR/step_${i}.pred.$STD_LANG\
             -batch_size 4000\
             -batch_type tokens\
             -beam_size 1\
             -replace_unk 
         source ./evaluate.sh $SRCSTD_MODELDIR/step_${i}.pred.$STD_LANG $SRC_LANG-$STD_LANG/bpetok/valid.$STD_LANG.bpetok $STD_LANG
-    else
-        echo "checkpoint $i not found"
+        #else
+       # echo "checkpoint $i not found"
     fi    
 done
